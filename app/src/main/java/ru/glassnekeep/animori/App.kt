e3851296b53470dde980832fe8104c523fad519d
glassnekeep <glassnekeep@yandex.ru>
2022-10-05 21:34:47 +0300

    Started implementing navigation

---
+++ app/src/main/java/ru/glassnekeep/animori/App.kt
@@ -0,0 +1,274 @@
+package ru.glassnekeep.animori
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.*
+import androidx.compose.material3.windowsizeclass.WindowHeightSizeClass
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.launch
+import ru.glassnekeep.core.ContentType
+import ru.glassnekeep.core.DevicePosture
+import ru.glassnekeep.core.NavigationContentPosition
+import ru.glassnekeep.core.NavigationType
+import ru.glassnekeep.navigation.*
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun App(
+    windowSize: WindowSizeClass,
+    foldingDevicePosture: DevicePosture,
+    replyHomeUIState: ReplyHomeUIState,
+    closeDetailScreen: () -> Unit = {},
+    navigateToDetail: (Long, ContentType) -> Unit = { _, _ -> }
+) {
+    /**
+     * This will help us select type of navigation and content type depending on window size and
+     * fold state of the device.
+     *
+     * In the state of folding device If it's half fold in BookPosture we want to avoid content
+     * at the crease/hinge
+     */
+    val navigationType: NavigationType
+    val contentType: ContentType
+
+    when (windowSize.widthSizeClass) {
+        WindowWidthSizeClass.Compact -> {
+            navigationType = NavigationType.BOTTOM_NAVIGATION
+            contentType = ContentType.SINGLE_PANE
+        }
+        WindowWidthSizeClass.Medium -> {
+            navigationType = NavigationType.NAVIGATION_RAIL
+            contentType = if (foldingDevicePosture != DevicePosture.NormalPosture) {
+                ContentType.DUAL_PANE
+            } else {
+                ContentType.SINGLE_PANE
+            }
+        }
+        WindowWidthSizeClass.Expanded -> {
+            navigationType = if (foldingDevicePosture is DevicePosture.BookPosture) {
+                NavigationType.NAVIGATION_RAIL
+            } else {
+                NavigationType.PERMANENT_NAVIGATION_DRAWER
+            }
+            contentType = ContentType.DUAL_PANE
+        }
+        else -> {
+            navigationType = NavigationType.BOTTOM_NAVIGATION
+            contentType = ContentType.SINGLE_PANE
+        }
+    }
+
+    /**
+     * Content inside Navigation Rail/Drawer can also be positioned at top, bottom or center for
+     * ergonomics and reachability depending upon the height of the device.
+     */
+    val navigationContentPosition = when (windowSize.heightSizeClass) {
+        WindowHeightSizeClass.Compact -> {
+            NavigationContentPosition.TOP
+        }
+        WindowHeightSizeClass.Medium,
+        WindowHeightSizeClass.Expanded -> {
+            NavigationContentPosition.CENTER
+        }
+        else -> {
+            NavigationContentPosition.TOP
+        }
+    }
+
+    NavigationWrapper(
+        navigationType = navigationType,
+        contentType = contentType,
+        navigationContentPosition = navigationContentPosition,
+        replyHomeUIState = replyHomeUIState,
+        closeDetailScreen = closeDetailScreen,
+        navigateToDetail = navigateToDetail
+    )
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun NavigationWrapper(
+    navigationType: NavigationType,
+    contentType: ContentType,
+    navigationContentPosition: NavigationContentPosition,
+    replyHomeUIState: ReplyHomeUIState,
+    closeDetailScreen: () -> Unit,
+    navigateToDetail: (Long, ContentType) -> Unit
+) {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    val navController = rememberNavController()
+    val navigationActions = remember(navController) {
+        NavigationActions(navController)
+    }
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val selectedDestination =
+        navBackStackEntry?.destination?.route ?: Routes.HOME
+
+    if (navigationType == NavigationType.PERMANENT_NAVIGATION_DRAWER) {
+        // TODO check on custom width of PermanentNavigationDrawer: b/232495216
+        PermanentNavigationDrawer(drawerContent = {
+            NavigationDrawerContent(
+                selectedDestination = selectedDestination,
+                isPermanentDrawer = true,
+                navigationContentPosition = navigationContentPosition,
+                navigateToTopLevelDestination = navigationActions::navigateTo,
+            )
+        }) {
+            AppContent(
+                navigationType = navigationType,
+                contentType = contentType,
+                navigationContentPosition = navigationContentPosition,
+                replyHomeUIState = replyHomeUIState,
+                navController = navController,
+                selectedDestination = selectedDestination,
+                navigateToTopLevelDestination = navigationActions::navigateTo,
+                closeDetailScreen = closeDetailScreen,
+                navigateToDetail = navigateToDetail
+            )
+        }
+    } else {
+        ModalNavigationDrawer(
+            drawerContent = {
+                NavigationDrawerContent(
+                    selectedDestination = selectedDestination,
+                    navigationContentPosition = navigationContentPosition,
+                    navigateToTopLevelDestination = navigationActions::navigateTo,
+                    onDrawerClicked = {
+                        scope.launch {
+                            drawerState.close()
+                        }
+                    }
+                )
+            },
+            drawerState = drawerState
+        ) {
+            ReplyAppContent(
+                navigationType = navigationType,
+                contentType = contentType,
+                navigationContentPosition = navigationContentPosition,
+                replyHomeUIState = replyHomeUIState,
+                navController = navController,
+                selectedDestination = selectedDestination,
+                navigateToTopLevelDestination = navigationActions::navigateTo,
+                closeDetailScreen = closeDetailScreen,
+                navigateToDetail = navigateToDetail
+            ) {
+                scope.launch {
+                    drawerState.open()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AppContent(
+    modifier: Modifier = Modifier,
+    navigationType: NavigationType,
+    contentType: ContentType,
+    navigationContentPosition: NavigationContentPosition,
+    replyHomeUIState: ReplyHomeUIState,
+    navController: NavHostController,
+    selectedDestination: String,
+    navigateToTopLevelDestination: (TopLevelDestinations) -> Unit,
+    closeDetailScreen: () -> Unit,
+    navigateToDetail: (Long, ContentType) -> Unit,
+    onDrawerClicked: () -> Unit = {}
+) {
+    Row(modifier = modifier.fillMaxSize()) {
+        AnimatedVisibility(visible = navigationType == NavigationType.NAVIGATION_RAIL) {
+            ReplyNavigationRail(
+                selectedDestination = selectedDestination,
+                navigationContentPosition = navigationContentPosition,
+                navigateToTopLevelDestination = navigateToTopLevelDestination,
+                onDrawerClicked = onDrawerClicked,
+            )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.inverseOnSurface)
+        ) {
+            ReplyNavHost(
+                navController = navController,
+                contentType = contentType,
+                replyHomeUIState = replyHomeUIState,
+                navigationType = navigationType,
+                closeDetailScreen = closeDetailScreen,
+                navigateToDetail = navigateToDetail,
+                modifier = Modifier.weight(1f),
+            )
+            AnimatedVisibility(visible = navigationType == NavigationType.BOTTOM_NAVIGATION) {
+                BottomNavigationBar(
+                    selectedDestination = selectedDestination,
+                    navigateToTopLevelDestinations = navigateToTopLevelDestination
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun ReplyNavHost(
+    navController: NavHostController,
+    contentType: ContentType,
+    replyHomeUIState: ReplyHomeUIState,
+    navigationType: NavigationType,
+    closeDetailScreen: () -> Unit,
+    navigateToDetail: (Long, ContentType) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = Routes.HOME,
+    ) {
+        composable(Routes.HOME) {
+            ReplyInboxScreen(
+                contentType = contentType,
+                replyHomeUIState = replyHomeUIState,
+                navigationType = navigationType,
+                closeDetailScreen = closeDetailScreen,
+                navigateToDetail = navigateToDetail,
+            )
+        }
+        composable(Routes.HOME) {
+            TODO()
+        }
+        composable(Routes.SEARCH) {
+            TODO()
+        }
+        composable(Routes.MYLIST) {
+            TODO()
+        }
+        composable(Routes.PROFILE) {
+            TODO()
+        }
+    }
+}
+
+data class ReplyHomeUIState(
+    val emails: List<Email> = emptyList(),
+    val selectedEmail: Email? = null,
+    val isDetailOnlyOpen: Boolean = false,
+    val loading: Boolean = false,
+    val error: String? = null
+)
+
