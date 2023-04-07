eb210cb075fb9d96f8179e5d0784290e6ad152e3
glassnekeep <56785261+glassnekeep@users.noreply.github.com>
2023-04-07 23:49:57 +0300

    Started creating AuthViewModel for registering the user

--- auth/src/main/java/ru/glassnekeep/auth/AuthViewModel.kt
+++ auth/src/main/java/ru/glassnekeep/auth/AuthViewModel.kt
@@ -1,9 +1,41 @@
 package ru.glassnekeep.auth
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import ru.glassnekeep.user_data.models.User
 import javax.inject.Inject
 
 class AuthViewModel @Inject constructor(
-    private val
+    private val registerUserUseCase: RegisterUserUseCase
 ): ViewModel() {
+    sealed class AuthState {
+        object Loading: AuthState()
+        object Success: AuthState()
+        data class Error(val message: String): AuthState()
+    }
+
+    private var _state = MutableStateFlow<AuthState>(AuthState.Loading)
+    val state = _state.asStateFlow()
+    val handler = CoroutineExceptionHandler { _, exception ->
+        Log.e(ERROR_TAG, exception.message ?: ERROR_MSG)
+        _state.value = AuthState.Error(ERROR_MSG)
+    }
+
+    fun register(user: User) {
+        viewModelScope.launch(handler) {
+            registerUserUseCase.execute(user)
+        }.invokeOnCompletion {
+            _state.value = AuthState.Success
+        }
+    }
+
+    companion object {
+        const val ERROR_MSG = "Error registering user"
+        const val ERROR_TAG = "REGISTER_ERROR"
+    }
 }
--- auth/src/main/java/ru/glassnekeep/auth/RegisterUserUseCase.kt
+++ auth/src/main/java/ru/glassnekeep/auth/RegisterUserUseCase.kt
@@ -1,19 +1,15 @@
 package ru.glassnekeep.auth
 
-import kotlinx.coroutines.withContext
-import ru.glassnekeep.core.di.AppDispatchers
 import ru.glassnekeep.core.di.AppScope
 import ru.glassnekeep.user_data.UserRepository
+import ru.glassnekeep.user_data.models.User
 import javax.inject.Inject
 
 @AppScope
 class RegisterUserUseCase @Inject constructor(
     private val userRepository: UserRepository,
-    private val dispatchers: AppDispatchers
 ) {
-    suspend fun execute() {
-        withContext(dispatchers.io) {
-            userRepository
-        }
+    suspend fun execute(user: User) {
+        userRepository.addUser(user)
     }
 }
